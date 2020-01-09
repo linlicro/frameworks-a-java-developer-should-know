27409c57f0494505e46dbf2ac06af7f1fb8bc456
linlicro <linli.cro@gmail.com>
2020-01-09 17:28:29 +0800

    feat: spring boot scaffold

---
+++ springBootscaffold/src/main/java/me/icro/java/springboot/scaffold/ScaffoldApplication.java
@@ -0,0 +1,17 @@
+package me.icro.java.springboot.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述:
+ *
+ * @author Lin
+ * @since 2020-01-09 2:54 PM
+ */
+@SpringBootApplication
+public class ScaffoldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffoldApplication.class, args);
+    }
+}
---
+++ springBootscaffold/src/main/java/me/icro/java/springboot/scaffold/config/CacheConfig.java
@@ -0,0 +1,93 @@
+package me.icro.java.springboot.scaffold.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+/**
+ * 描述: redis缓存配置
+ *
+ * @author Lin
+ * @since 2020-01-09 3:49 PM
+ */
+@Configuration
+public class CacheConfig {
+
+    /**
+     * redis序列化问题(默认采用的是JDK序列化)
+     * https://docs.spring.io/spring-data/redis/docs/2.1.14.RELEASE/reference/html/#redis:serializer
+     * @param factory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(factory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        // key 采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // hash 采用String序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        template.setValueSerializer(jackson2JsonRedisSerializer);
+        // hash的value序列化采用jackson
+        template.setHashValueSerializer(jackson2JsonRedisSerializer);
+
+        template.afterPropertiesSet();
+        return template;
+    }
+
+
+    /**
+     * 全局缓存策略
+     * https://docs.spring.io/spring-data/redis/docs/2.1.14.RELEASE/reference/html/#redis:support:cache-abstraction
+     * @param factory
+     * @return
+     */
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory factory) {
+
+        // 设置全局redis缓存过期时间为10天
+        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        //解决查询缓存转换异常的问题
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        // 配置序列化（解决乱码的问题）
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                // 10天过期
+                .entryTtl(Duration.ofDays(10))
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
+                .disableCachingNullValues();
+
+        RedisCacheManager cacheManager = RedisCacheManager.builder(factory)
+                .cacheDefaults(config)
+                .build();
+        return cacheManager;
+
+    }
+}
